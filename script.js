bb669df405118cfa675656395f1ef3b47ff0f2d3
rogerxavier29 <rogerdpx@gmail.com>
2021-08-27 08:26:10 -0300

    colocado as idades dentro de um array e pegado a maior idade e fazendo a comparação entre uma e outra.

--- script.js
+++ script.js
@@ -39,3 +39,93 @@ function maior() {
   var resultadoMax = valorMax;
   document.querySelector('.resultadoMax').innerHTML = resultadoMax;
 }
+
+function idadeMax() {
+  var idadis1 = document.querySelector('.idadis1').value;
+  var idadis2 = document.querySelector('.idadis2').value;
+  var idadis3 = document.querySelector('.idadis3').value;
+  var idadis4 = document.querySelector('.idadis4').value;
+
+  if (idadis1 >= 18) {
+    var idadeMaior1 = idadis1;
+  } else {
+    var idadeMenor1 = idadis1;
+  }
+
+  if (idadis2 >= 18) {
+    var idadeMaior2 = idadis2;
+  } else {
+    var idadeMenor2 = idadis2;
+  }
+
+  if (idadis3 >= 18) {
+    var idadeMaior3 = idadis3;
+  } else {
+    var idadeMenor3 = idadis3;
+  }
+
+  if (idadis4 >= 18) {
+    var idadeMaior4 = idadis4;
+  } else {
+    var idadeMenor4 = idadis4;
+  }
+
+  idadesMaiores.push(idadeMaior1, idadeMaior2, idadeMaior3, idadeMaior4);
+
+  idadesMenores.push(idadeMenor1, idadeMenor2, idadeMenor3, idadeMenor4);
+
+  var quantIdadeMax = idadesMaiores.lenght;
+  var quantIdadeMen = idadesMenores.lenght;
+  document.querySelector('.resultadoIdades').innerHTML =
+    'Idades maximas =' + quantIdadeMax + ' E idades menores =' + quantIdadeMen;
+}
+
+///Exercicio 5
+
+function altura() {
+  var quantPessoas = 0;
+  var qtdAlturas = [];
+  var alturaF = 0;
+  var alturaM = 0;
+  var pessoasF = 0;
+  var pessoasM = 0;
+
+  while (quantPessoas < 4) {
+    var sexo = prompt('Digite sexo da pessoa ' + (quantPessoas + 1));
+    var altura = parseFloat(
+      prompt('Digite a ALTURA da Pessoa ' + (totalPessoas + 1)),
+    );
+
+    if (sexo == 'F' || sexo == 'f') {
+      qtdAlturas.push(altura);
+      alturaF = alturaF + altura;
+      console.log(alturaF);
+      pessoasF++;
+      console.log(pessoasF);
+    } else {
+      if (sexo == 'M' || sexo == 'm') {
+        qtdAlturas.push(altura);
+        alturaM = alturaM + altura;
+        console.log(alturaM);
+        pessoasM++;
+        console.log(pessoasM);
+      }
+    }
+
+    quantPessoas++;
+  }
+
+  function maiores() {
+    var maiorAlturaM = Math.max.apply(null, alturaM);
+    var maiorAlturaF = Math.max.apply(null, alturaF);
+    if (maiorAlturaM > maiorAlturaF) {
+      console.log('A maior altura é de um homem' + maiorAlturaM);
+    } else {
+      console.log('A maior altura é de um homem' + maiorAlturaF);
+    }
+  }
+
+  document.write('Quantidade de mulheres' + pessoasF);
+
+  document.write('Quantidade de homem: ' + pessoasM);
+}
