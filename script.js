baf78930a473eafe893f58eab8809e44291b2e73
rogerxavier29 <rogerdpx@gmail.com>
2021-09-13 10:14:47 -0300

    feito o exercicio 44, o contador

--- script.js
+++ script.js
@@ -629,9 +629,32 @@ function array8() {
   list.appendChild(li);
 }
 
+// Exercicio 43
+// function array9() {
+//   var elemento = document.querySelector('.input-pares').value;
+//   const pares = document.querySelector('.pares');
+//   const valuePares = elemento;
+//   var ul = document.querySelector('elemento');
+//   ul.innerHTML = '';
+
+//   for (i in elemento) {
+//     var li = document.createElement('li');
+//     var el = document.createTextNode(elemento[i]);
+//     li.appendChild(el);
+
+//     if (elemento[i] % 2 == 0) {
+//       //par
+//       ul.appendChild(li).setAttribute('class', 'par');
+//     } else {
+//       ul.appendChild(li);
+//     }
+//   }
+// }
+
 // Exercicio 44
+function cont2() {
+  let time = Math.random();
+  document.querySelector('.contador5').innerHTML = time;
+}
 
-// var elemento = document.querySelector('.contador');
-// var contador = 0;
-
-// setInterval(() => (elemento.innerHTML = contador++), 1000);
+const contador = setInterval(displayTime, 1000);
